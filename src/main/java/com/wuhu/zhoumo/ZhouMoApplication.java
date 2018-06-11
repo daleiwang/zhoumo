68c75e35d8059702ba45b0ca0f6b7bd4f9922f2a
鼠大雷 <>
2018-06-11 18:13:24 +0800

    提交

--- src/main/java/com/wuhu/zhoumo/ZhouMoApplication.java
+++ src/main/java/com/wuhu/zhoumo/ZhouMoApplication.java
@@ -3,6 +3,9 @@ package com.wuhu.zhoumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author shudalei
+ */
 @SpringBootApplication
 public class ZhouMoApplication {
 
---
+++ src/main/java/com/wuhu/zhoumo/dao/AdminDAO.java
@@ -0,0 +1,13 @@
+package com.wuhu.zhoumo.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author sdl
+ * @date 2018/6/11
+ */
+@Mapper
+public class AdminDAO {
+
+
+}
