77266c7a7a64d87ac4d7276f78bc681b409e0608
鼠大雷 <>
2018-06-11 16:21:30 +0800

    第一次提交

---
+++ src/main/java/com/wuhu/zhoumo/ZhouMoApplication.java
@@ -0,0 +1,12 @@
+package com.wuhu.zhoumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhouMoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhouMoApplication.class, args);
+    }
+}
